e6f4781afaab5d1297a9c9e451a60fa4f87a8dd0
Artem Sheremet <dot.doom@gmail.com>
2025-10-12 10:15:17 +0000

    Move from venv to Nix

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "esphome deployment shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            esphome
+          ];
+
+          shellHook = ''
+            echo -n "ESPHome "
+            esphome --version
+            exec zsh
+          '';
+        };
+      }
+    );
+}
